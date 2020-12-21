8f687b5eccf0488a07fd23996f79a98be2844191
Mario Pagnozzi <mario.antonio.pagnozzi30@gmail.com>
2020-12-21 11:32:14 -0300

    #39 issue 23

--- atacadoApp/atacadoApp_API/src/controller/GrupoController.ts
+++ atacadoApp/atacadoApp_API/src/controller/GrupoController.ts
@@ -1,10 +1,8 @@
 import { Request } from 'express';
-import { getRepository, Repository } from 'typeorm';
 import { Grupos } from './../entity/Grupos';
 import { BaseController } from "./BaseController";
 
 export class GrupoController extends BaseController<Grupos> {
-    private _repGrupo: Repository<Grupos> = getRepository(Grupos);
     constructor () {
         super(Grupos);
     }
@@ -12,8 +10,6 @@ export class GrupoController extends BaseController<Grupos> {
         let grupo = <Grupos>request.body;
 
         super.isRequired(grupo.nome_grupo, "O 'Nome do Grupo' deve ser informado!");
-        super.isRequired(grupo.permissoes, "A 'Permissão' do grupo deve ser configurada!");
-
         return super.save(grupo);
     }
 }
