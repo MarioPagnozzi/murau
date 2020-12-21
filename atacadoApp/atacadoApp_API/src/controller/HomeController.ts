c27fe9c408904c332e6f58893ca924530c0abe53
Mario Pagnozzi <mario.antonio.pagnozzi30@gmail.com>
2020-12-21 16:33:08 -0300

    Controller Home Fixes #22

--- atacadoApp/atacadoApp_API/src/controller/HomeController.ts
+++ atacadoApp/atacadoApp_API/src/controller/HomeController.ts
@@ -1,8 +1,22 @@
+import { Request } from 'express';
+import { getRepository, Like, Repository } from 'typeorm';
 import { Produtos } from './../entity/Produtos';
 import { BaseController } from "./BaseController";
 
 export class HomeController extends BaseController<Produtos> {
+    private _repProdutos: Repository<Produtos> = getRepository(Produtos);
     constructor() {
         super(Produtos);
+    }
+    async Pesquisa(request: Request) {
+        return this._repProdutos.find({
+            where: [
+                [{codigo: request.params.valor},
+                {nome: Like("%" + request.params.valor + "%")},
+                {referencia: Like("%" + request.params.valor + "%")}], 
+                {empresa: {codigo: request.params.empresa}}
+                
+            ]
+        })
     }
 }
