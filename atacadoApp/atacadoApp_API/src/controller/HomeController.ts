7724e1620ab7510bc1d21bdad268f884024cb019
Mario Pagnozzi <mario.antonio.pagnozzi30@gmail.com>
2020-12-21 18:14:07 -0300

    Controller Home Fixes #22

--- atacadoApp/atacadoApp_API/src/controller/HomeController.ts
+++ atacadoApp/atacadoApp_API/src/controller/HomeController.ts
@@ -9,14 +9,27 @@ export class HomeController extends BaseController<Produtos> {
         super(Produtos);
     }
     async Pesquisa(request: Request) {
-        return this._repProdutos.find({
-            where: [
-                [{codigo: request.params.valor},
-                {nome: Like("%" + request.params.valor + "%")},
-                {referencia: Like("%" + request.params.valor + "%")}], 
-                {empresa: {codigo: request.params.empresa}}
-                
-            ]
-        })
+        return this._repProdutos.createQueryBuilder("produtos").addSelect("produtos.*")
+                                .innerJoin("produtos.produtosEmpresas", "produtosEmpresas").addSelect([
+                                    "produtosEmpresas.valor",
+                                    "produtosEmpresas.estoque"
+                                ])
+                                .innerJoin("produtosEmpresas.empresa", "empresa").addSelect([
+                                    "empresa.codigo",
+                                    "empresa.razao_social",
+                                    "empresa.nome_fantasia",
+                                    "empresa.cnpj",
+                                    "empresa.cep",
+                                    "empresa.endereco",
+                                    "empresa.numero",
+                                    "empresa.bairro",
+                                    "empresa.cidade",
+                                    "empresa.uf"
+                                ])
+                                .where("(empresa.codigo = :codigo)", {codigo: request.params.empresa})
+                                .andWhere("(produtos.codigo = :codprod", {codprod: request.params.valor})
+                                .orWhere("produtos.referencia like (:referencia)", {referencia: '%' + request.params.valor + '%'})
+                                .orWhere("produtos.nome like (:nome))", {nome: '%' + request.params.valor + '%'})
+                                .getMany();
     }
-}
+} 
